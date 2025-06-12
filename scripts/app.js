4ae789f57c2b8d287ff1b2e016ef8dc1564fa9ce
dancnguyen <71113769+dancnguyen@users.noreply.github.com>
2025-06-12 22:35:32 +0000

    Deploying to gh-pages from  @ 7747c50bde8c78a041d23f2163d77345eb50bec9 🚀

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+﻿window.downloadFile = (filename, content) => {
+  const blob = new Blob([content], { type: 'text/csv' });
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
